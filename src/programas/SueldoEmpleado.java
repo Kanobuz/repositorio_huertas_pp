4d35d714087db83065b4f902834eb6b49e2e7849
Said Huertas <shuertas1992@gmail.com>
2025-01-16 20:55:35 -0500

    Inicio de Proyecto

---
+++ src/programas/SueldoEmpleado.java
@@ -0,0 +1,35 @@
+package programas;
+
+/**
+ * @author Huertas Quiroz Elver Said Ney
+ */
+import java.util.Scanner;
+
+public class SueldoEmpleado {
+    public static void main(String args[]) {
+        // Declaración de Variables
+        String empleado;
+        double sueldoBruto, horasTrabajadas, tarifaHora, descuento, sueldoNeto;
+        
+        Scanner lectura = new Scanner(System.in);
+        
+        // Entrada de Datos
+        System.out.print("Nombre de empleado: ");
+        empleado = lectura.next();
+        System.out.print("Horas trabajadas: ");
+        horasTrabajadas = lectura.nextDouble();
+        System.out.print("Tarifa por Hora: ");
+        tarifaHora = lectura.nextDouble();
+        
+        // Proceso de Datos
+        sueldoBruto = horasTrabajadas * tarifaHora;
+        descuento = sueldoBruto * 0.13;
+        sueldoNeto = sueldoBruto - descuento;
+        
+        // Salida de Datos
+        System.out.println("Sueldo Bruto: " + sueldoBruto);
+        System.out.println("Descuento: " + descuento);
+        System.out.println("Sueldo Neto: " + sueldoNeto);
+        
+    }
+}
